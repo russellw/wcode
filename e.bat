44cc1a39854e29dbd4f4eda59b7c9267fa969094
Russell Wallace <russell.wallace@gmail.com>
2025-07-08 04:13:59 +0100

    Modified 1 file(s): e.bat

--- e.bat
+++ e.bat
@@ -1,2 +1,2 @@
-echo I just ran `dotnet run --project wcode.Cli example/instructions.txt example|tee log.txt`. Look in example/ and log.txt for the results |clip
+echo I just ran `dotnet run --project wcode.Cli example/instructions.txt example` and directed output to log.txt. Look in example/ and log.txt for the results |clip
 dotnet run --project wcode.Cli example/instructions.txt example|tee log.txt
