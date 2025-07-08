c15a8572f44b9b7d5323d74e13f5dcf24b0606d9
Russell Wallace <russell.wallace@gmail.com>
2025-07-08 03:00:21 +0100

    Added 1 file(s): e.bat

---
+++ e.bat
@@ -0,0 +1,2 @@
+echo I just ran `dotnet run --project wcode.Cli example\instructions.txt|tee example\output.txt`. Look at example\output.txt for the results |clip
+dotnet run --project wcode.Cli example\instructions.txt|tee example\output.txt
