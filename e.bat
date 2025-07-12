5f3b72a98db5a01796db11f895b2085f97e1d57b
Russell Wallace <russell.wallace@gmail.com>
2025-07-12 07:07:05 +0100

    Modified 1 file(s): e.bat

--- e.bat
+++ e.bat
@@ -1,2 +1,5 @@
+rd /s /q example||exit /b
+md example||exit /b
+copy example_instructions.txt example\instructions.txt||exit /b
 echo I just ran `dotnet run --project wcode.Cli example/instructions.txt example` and directed output to log.txt. Look in example/ and log.txt for the results |clip
 dotnet run --project wcode.Cli example/instructions.txt example|tee log.txt
